bc9643d3d4abeaa93bd303952e2718a668edf608
Olya Poltavec <poltavec.olya@gmail.com>
2020-06-07 17:25:56 +0300

    Added Entities to DAL

---
+++ DAL/Entities/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL.Entities
+{
+	public class User
+	{
+		public int Id { get; set; }
+		public string Login { get; set; }
+		public string Password { get; set; }
+		public bool IsAdmin { get; set; }
+		public string Email { get; set; }
+	}
+}
