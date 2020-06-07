75b42073905959932335edf7edbaf93488ade259
Olya Poltavec <poltavec.olya@gmail.com>
2020-06-07 17:29:08 +0300

    Added Contracts of Repositories

---
+++ DAL/Repositories/Interfaces/IUserRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DAL.Entities;
+
+namespace DAL.Repositories.Interfaces
+{
+	public interface IUserRepository : IRepository<User>
+	{
+	}
+}
